7965b9e6f331f22b4b4910eab4bba8d01dc81d37
Kako Ferrare <kakoferrare87@gmail.com>
2023-12-08 14:37:11 -0300

    refactor: adicionado anotação para json object no input de credito

--- Domain/Records/PropostaCredito.cs
+++ Domain/Records/PropostaCredito.cs
@@ -1,8 +1,18 @@
 ﻿using Domain.Enums;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace Domain.Records
 {
-    public record PropostaCredito(string Cpf, string Nome, string UF, string Celular, eTipoCredito TipoCredito, decimal ValorCredito, int QuantidadeParcelas, DateTime DataPrimeiroVencimento)
+    public record PropostaCredito(
+        string Cpf, 
+        string Nome,
+        [property: JsonProperty("uf"), JsonPropertyName("uf")] string UF, 
+        string Celular,
+        [property: JsonProperty("tipo_credito"), JsonPropertyName("tipo_credito")] eTipoCredito TipoCredito,
+        [property: JsonProperty("valor_credito"), JsonPropertyName("valor_credito")] decimal ValorCredito,
+        [property: JsonProperty("quantidade_parcelas"), JsonPropertyName("quantidade_parcelas")] int QuantidadeParcelas,
+        [property: JsonProperty("data_primeiro_vencimento"), JsonPropertyName("data_primeiro_vencimento")] DateTime DataPrimeiroVencimento)
     {
     }
 }
